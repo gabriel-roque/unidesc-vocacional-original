9fba90a654a8787a72602e00515d0fc796d4c49d
Jadilson Guedes <jadilson12@gmail.com>
2019-05-20 08:41:06 -0300

    [UPDATE/REMOVE]:  Limpando e adicionando a nova rota

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['namespace' => 'APIs'], function (){
-    Route::get('/quest/enum', 'APIQuestaoController@JsonQuestoesEnum');
-    Route::get('/quest/op', 'APIQuestaoController@JsonQuestoesOp');
+    Route::get('/questoes', 'APIQuestaoController@JsonQuestoes');
+
 });
